318cddfa93145e57b653d8a52fc99eda4a7d1ffc
Cary Wolff <boss@caryyon.com>
2022-08-29 01:18:42 -0500

    slight refactor and parser

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
 use walkdir::WalkDir;
+//use serde::{Serialize, Deserialize};
+use serde_json::json;
+use std::path::Path;
 use std::fs;
+use graphql_parser::query::parse_query;
 
 fn main() {
     const PATH: &str = "src/etc";
-
     for file in WalkDir::new(PATH).into_iter().filter_map(|file| file.ok()) {
-        //probably a less gross way to write this...
-        //check if its a file and has a .graphql extension
-        if file.metadata().unwrap().is_file() && file.file_name().to_str().map(|s| s.ends_with(".graphql")).unwrap_or(false) {
-            let path = file.path();
-            println!("graphql file path {}", path.display());
-            let data = fs::read_to_string(path).expect("Unable to read file");
-            println!("contents {}", data);
+        let extension = Path::new(file.file_name()).extension();
+        match extension {
+            Some(val) => {
+                let ext = val.to_str().unwrap();
+                if ext == "graphql" || ext == "gql" {
+                    let path = file.path();
+                    let data = fs::read_to_string(path).expect("Unable to read file");
+                    // parse graphql query to ast
+                    let ast = parse_query::<&str>(data.as_str()).unwrap();
+                    // TODO: add visitor pattern here
+                    // you can change the following {:?} to {:#?} for different fmting
+                    // we are gonna have to parse the files to ASTs because they're
+                    // not .js or .json
+                    //
+                    // is we find/write a visitor pattern to handle this
+                    // some options for other graphql crates
+                    // https://github.com/graphql-rust/graphql-client
+                    let json = json!(format!("{:?}", ast));
+                    println!("{}", json);
+                }
+            }
+            None => ()
         }
     }
 }
