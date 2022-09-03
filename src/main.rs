e06ed462d8b5460b95ec740fd01ae3f20ff517a2
Cary Wolff <boss@caryyon.com>
2022-09-02 22:54:11 -0500

    lots more layout work

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,59 @@
+use std::io::Error;
 use walkdir::WalkDir;
-use serde_json::{json,Value, to_writer_pretty};
+use serde::{Serialize, Deserialize};
+use serde_json::{json, to_writer_pretty, from_value};
 use std::path::Path;
 use std::fs;
 use std::fs::File;
-use std::io::prelude::*;
 use graphql_parser::query::parse_query;
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Url {
+    raw: String,
+    protocol: String,
+    host: Vec<String>,
+    path: Vec<String>
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct Graphql {
+    query: String,
+    variables: String
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct Body {
+    mode: String,
+    graphql: Graphql
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct Request {
+    method: String,
+    header: Vec<String>,
+    body: Body,
+    url: Url
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct Query {
+    name: String,
+    request: Request,
+    response: Vec<String>
 
+}
 
-fn main() {
+fn main() -> Result<(), Error> {
     const PATH: &str = "src/etc";
     for file in WalkDir::new(PATH).into_iter().filter_map(|file| file.ok()) {
         let extension = Path::new(file.file_name()).extension();
         match extension {
             Some(val) => {
-                let ext = val.to_str().unwrap();
+                let ext = val.to_str().expect("failed to get file with that extension");
                 if ext == "graphql" || ext == "gql" {
                     let path = file.path();
                     let data = fs::read_to_string(path).expect("Unable to read file");
                     // parse graphql query to ast
-                    let ast = parse_query::<&str>(data.as_str()).unwrap();
+                    let ast = parse_query::<&str>(data.as_str()).expect("failed to parse the file");
 
                     // transform ast to json
-                    let json_template: Value = json!({
+                    let query: Query = from_value(json!({
                         "name": "character",
                         "request": {
                             "method": "POST",
@@ -47,18 +78,18 @@ fn main() {
                             }
                         },
                         "response": []
-                    });
-
-
-
+                    }))?;
+                    //println!("{:#?}", q);
 
                     // write to file
-                    let file = File::create("collection.json").unwrap();
-                    to_writer_pretty(file, &json_template).unwrap();
+                    let file = File::create("collection.json").expect("couldn't create file");
+                    to_writer_pretty(file, &query).expect("failed to write to file");
                 }
             }
             None => ()
         }
     }
+    Ok(())
+
 }
 
