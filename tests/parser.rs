ae8dd17beb2c05e8bf4f4d526b644ad44380da28
Stacy Adam <stacykelnhofer@northwesternmutual.com>
2022-09-03 09:57:11 -0500

    first pass at making code more testable

---
+++ tests/parser.rs
@@ -0,0 +1,9 @@
+use graphler::{
+    parser::{parse}
+};
+
+#[test]
+fn valid_collection_json() {
+    // todo: break up parser into parseing and file creation
+    // test parser output against a mocked valid postman.collection.json
+}
