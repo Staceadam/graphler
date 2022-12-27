ae049126b41391c589828d6b5ffb20d82896c65a
Stacy Adam <staceadam@gmail.com>
2022-12-27 13:02:59 -0600

    working query

--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,13 @@
+use crate::collection::{Collection, Query};
+use apollo_parser::{
+    ast::{self, NamedType, OperationDefinition},
+    Parser, TokenKind,
+};
+use std::collections::HashMap;
 use std::fs;
 use std::io::Error;
 use std::path::Path;
-use apollo_parser::{ast::{self, OperationDefinition, NamedType}, Parser, TokenKind};
 use walkdir::WalkDir;
-use crate::collection::{Collection, Query};
-use std::collections::HashMap;
-
 
 pub fn parse(path: &str) -> Result<Collection, Error> {
     let mut collection = Collection::new("insertNameFromCliInputOrUrlBase");
@@ -27,81 +29,95 @@ pub fn parse(path: &str) -> Result<Collection, Error> {
                     for def in doc.definitions() {
                         match def {
                             ast::Definition::FragmentDefinition(frag_def) => {
+                                // if there is a fragment add it to a hashmap/list that persists through each files lifetime
+                                // this might have to instead do an initial pass through all the files and only grab the fragments
+                                // then go through the definitions again without taking fragment definitions into consideration
                                 println!("{:?}", frag_def);
-                            },
+                            }
                             ast::Definition::OperationDefinition(op_def) => {
-                                // needs to check operation type for query/mutation/subscription
                                 let name = op_def.name().unwrap().text().as_str().to_string();
-                                
-						// "variables": "{\n\t\"page\": null,\n\t\"filter\": null\n}"
-                        //vs
-                        // graphql: Graphql {
-                        //     variables: "id name",
-                        // },
 
+                                // check if operation has any fragments in it
+                                // if so grab all fragments in operation and then check them against list of fragments to find a match
+                                // prepend entire fragment definition onto the data string that gets passed into query
+                                // println!("{:#?}", op_def.selection_set().unwrap().selections().any(f));
                                 // variables
                                 let variable_defs = op_def.variable_definitions();
                                 let flat = variable_defs
                                     .iter()
                                     .map(|v| v.variable_definitions())
                                     .flatten();
-                                
+
                                 //TODO: these need to check an existing collection before setting them to dummy values
-                                let mut variables = HashMap::new(); 
+                                let mut variables = HashMap::new();
 
                                 for var_def in flat {
                                     let name = var_def.variable().unwrap().text().to_string();
                                     match var_def.ty().unwrap() {
-                                       ast::Type::ListType(test) => {
-                                            variables.insert("list".to_string(), "idk".to_string());
-                                       },
-                                       ast::Type::NamedType(named) => {
-                                            let str_type = named.name().unwrap().ident_token().unwrap().text().to_string();
-                                            // type is nullable, set it to null
-                                            // TODO: need to set it as null not "null"
-                                            if str_type == "ID" {
-                                                variables.insert(name, "".to_string());
-                                            } else if str_type == "String" {
-                                                variables.insert(name, "".to_string());
-                                            } else if str_type == "Boolean" {
-                                                variables.insert(name, "".to_string());
-                                            } else if str_type == "Float" {
-                                                variables.insert(name, "".to_string());
-                                            } else if str_type == "Int" {
-                                                variables.insert(name, "".to_string());
-                                            } else {
-                                                //TODO: check other types and create object or list
-                                                variables.insert(name, "null".to_string());
-                                            }
-                                            // buf.push_str(&var_def.variable().unwrap().text().to_string());
-                                            // buf.push_str(&named.name().unwrap().ident_token().unwrap().to_string());
-                                        // println!("{:#?}", named.name().unwrap().ident_token().unwrap().to_string())
-                                       },
-                                       ast::Type::NonNullType(non_null) => {
-                                        let str_type = non_null.named_type().unwrap().name().unwrap().ident_token().unwrap().text().to_string();
-
-                                        if str_type == "ID" {
-                                            variables.insert(name, "0".to_string());
-                                        } else if str_type == "String" {
-                                            variables.insert(name, "test".to_string());
-                                        } else if str_type == "Boolean" {
-                                            variables.insert(name, "false".to_string());
-                                        } else if str_type == "Float" {
-                                            variables.insert(name, "1.2".to_string());
-                                        } else if str_type == "Int" {
-                                            variables.insert(name, "1".to_string());
-                                        } else {
-                                            //TODO: check other types and create object or list
-                                            variables.insert(name, "unique".to_string());
+                                        ast::Type::ListType(list_type) => {
+                                            //TODO: need to dig into how to resolve a list, that is something that comes in as [String] etc...
+                                            /*
+                                                "variables":{"ids":["0"]
+                                            */
+                                            variables.insert(name, Some("idk".to_string()));
                                         }
-                                       }
+                                        ast::Type::NamedType(named) => {
+                                            let str_type = named
+                                                .name()
+                                                .unwrap()
+                                                .ident_token()
+                                                .unwrap()
+                                                .text()
+                                                .to_string();
+
+                                            if str_type == "ID" {
+                                                variables.insert(name, None);
+                                            } else if str_type == "String" {
+                                                variables.insert(name, None);
+                                            } else if str_type == "Boolean" {
+                                                variables.insert(name, None);
+                                            } else if str_type == "Float" {
+                                                variables.insert(name, None);
+                                            } else if str_type == "Int" {
+                                                variables.insert(name, None);
+                                            } else {
+                                                //TODO: theres no real way to check what a unique types shape is
+                                                variables.insert(name, None);
+                                            }
+                                        }
+                                        ast::Type::NonNullType(non_null) => {
+                                            let str_type = non_null
+                                                .named_type()
+                                                .unwrap()
+                                                .name()
+                                                .unwrap()
+                                                .ident_token()
+                                                .unwrap()
+                                                .text()
+                                                .to_string();
+
+                                            if str_type == "ID" {
+                                                variables.insert(name, Some("0".to_string()));
+                                            } else if str_type == "String" {
+                                                variables.insert(name, Some("test".to_string()));
+                                            } else if str_type == "Boolean" {
+                                                variables.insert(name, Some("true".to_string()));
+                                            } else if str_type == "Float" {
+                                                variables.insert(name, Some("1.2".to_string()));
+                                            } else if str_type == "Int" {
+                                                variables.insert(name, Some("1".to_string()));
+                                            } else {
+                                                //TODO: theres no real way to check what a unique types shape is
+                                                variables.insert(name, Some("{}".to_string()));
+                                            }
+                                        }
                                     }
                                 }
                                 let query = Query::new(name, &data, variables);
 
                                 collection.item.push(query)
-                            },
-                            _ => println!("not found") 
+                            }
+                            _ => println!("not found"),
                         }
                     }
                 }
@@ -112,8 +128,8 @@ pub fn parse(path: &str) -> Result<Collection, Error> {
     Ok(collection)
 }
 
-
 #[test]
 fn test_info() {
     let parsed = parse("etc").expect("parsing failed");
+    println!("{:#?}", parsed)
 }
